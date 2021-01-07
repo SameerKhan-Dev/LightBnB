75fdd1e3ef08ad19867984283958fc6d34771cd7
Sameer Khan <sameerk_786@hotmail.com>
2021-01-07 06:39:49 +0000

    added more queries to 1_queries folder

---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,24 @@
+/*
+  Our product managers want a query to see a list of the most visited cities.
+
+  - Get a list of the most visited cities.
+
+  - Select the name of the city and the number of reservations for that city.
+  - Order the results from the highest number of reservations to lowest number of reservations.
+
+*/
+
+SELECT properties.city, count(reservations) as total_reservations
+FROM properties
+JOIN reservations ON properties.id = property_id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
+
+/*
+SELECT properties.city, count(reservations) as total_reservations
+FROM reservations
+JOIN properties ON property_id = properties.id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
+*/
+
